b5b36658a4915d222cb2931b0eedb5d76de83d48
unknown <asikkolorob@gmail.com>
2021-05-08 02:24:53 +0600

    new add

--- fsModule/index.js
+++ fsModule/index.js
@@ -4,7 +4,7 @@ var http=require('http');
 var server=http.createServer(function (req,res) {
 
     if(req.url=="/"){
-        //Asy
+        //Async
         fs.readFile('Home.html',function (error,data) {
             res.writeHead(200,{'Content-Type':'text/html'});
             res.write(data);
